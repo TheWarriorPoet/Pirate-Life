894b12125d86ecdee18215c0f44658543a729959
TheWarriorPoet <andrew.jones@students.aie.edu.au>
2015-10-15 19:16:27 +1100

    Player Die & Respawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    
+    // Singleton Instance to provide simple access through other scripts
+    private static GameManager _instance = null;
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (GameManager)FindObjectOfType(typeof(GameManager));
+            }
+            return _instance;
+        }
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SceneManager_Andrew.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager_Andrew : SceneManager_Base {
+    public int m_Lives = 3;
+    public GameObject m_PlayerObject = null;
+
+    private static SceneManager_Andrew _instance = null;
+    public static SceneManager_Andrew instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (SceneManager_Andrew)FindObjectOfType(typeof(SceneManager_Andrew));
+            }
+            return _instance;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AddLife(int a_Lives)
+    {
+        m_Lives += a_Lives;
+    }
+
+    public void Die()
+    {
+        m_Lives--;
+        if (m_Lives == 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over!!!");
+        Time.timeScale = 0;
+    }
+}
