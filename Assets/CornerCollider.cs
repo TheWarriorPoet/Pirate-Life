35e0870ab5e1386c0777c32e38f18a435ec2343e
Mitchell Osborne <MOsborne@live.com.au>
2015-10-15 14:29:56 -0700

    Created the Corner Platform prefab. If the player presses left or right (horizontal in the Input List), the player will rotate 90 degrees (to the left or right). Note: Rotation isn't visible because player is a box and looks identical 90 degrees. Note 2: The Players movement should be changed to use transform.forward, so the player will run forward based on rotation, rather than on a set axis.

---
+++ Assets/CornerCollider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CornerCollider : MonoBehaviour {
+	public GameObject Player;
+	bool hasRotated = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider Other)
+	{
+		if (Other.gameObject == Player) {
+			if(!hasRotated)
+			{
+				if(Input.GetAxis("Horizontal") < 0)
+				{
+					Player.transform.Rotate(new Vector3(0,90,0));
+					hasRotated = true;
+				}else if(Input.GetAxis ("Horizontal") > 0)
+				{
+					Player.transform.Rotate(new Vector3(0,90,0));
+					hasRotated = true;
+				}
+			}
+		}
+	}
+	void OnTriggerExit(Collider Other)
+	{
+		if (Other.gameObject == Player) {
+			hasRotated = false;
+		}
+	}
+}
