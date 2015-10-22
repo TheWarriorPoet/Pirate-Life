1ef04339923e94b6ca02c1d639ea67c03e220690
Mitchell Osborne <MOsborne@live.com.au>
2015-10-22 15:37:58 -0700

    Added a script to detect collision between Player and Pickups.

---
+++ Assets/PickupScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PickupType
+{
+	COIN,
+	RUM,
+	WATER,
+}
+public class PickupScript : MonoBehaviour {
+
+	public GameObject Player;
+	public PickupType pickupType;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if (Other.gameObject == Player) {
+			//Insert Reaction Code here
+		}
+	}
+}
