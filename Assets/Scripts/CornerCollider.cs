9902e102af26ec3e9797792be556a2f01bf247c7
Mitchell Osborne <MOsborne@live.com.au>
2015-10-22 15:11:05 -0700

    Added ultra saxxy pickup placeholders. Added Rum, Coin and Water pickup prefab, Added Rum, Coin, Glass and Water material for pickups

--- Assets/Scripts/CornerCollider.cs
+++ Assets/Scripts/CornerCollider.cs
@@ -30,24 +30,11 @@ public class CornerCollider : MonoBehaviour {
 			{
 				if(Input.GetAxis("Horizontal") < 0)
 				{
-
-					WorldContainer.transform.RotateAround(gameObject.transform.position, new Vector3(0,1,0), 90);
-					Vector3 dist = gameObject.transform.position - Player.transform.position;
-					if(dist.z > Player.GetComponent<Player>().laneDistance)
-					{
-						Player.GetComponent<Player>().currentLane = -1;
-					}
-					else if(dist.z < (Player.GetComponent<Player>().laneDistance * -1))
-					{
-						Player.GetComponent<Player>().currentLane = 1;
-					}
-					else{
-						Player.GetComponent<Player>().currentLane = 0;
-					}
+					Player.transform.Rotate(new Vector3(0,-90,0));
 					hasRotated = true;
 				}else if(Input.GetAxis ("Horizontal") > 0)
 				{
-					WorldContainer.transform.RotateAround(gameObject.transform.position, new Vector3(0,1,0), -90);
+					Player.transform.Rotate(new Vector3(0,90,0));
 					hasRotated = true;
 				}
 			}
