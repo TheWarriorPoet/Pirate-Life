76bba05b45678f3e37f2d73533d67195bff1a7b0
Mitchell Osborne <MOsborne@live.com.au>
2015-10-22 21:06:51 -0700

    Added the supa doopa Level Generation stuff. Added the Map Piece script, which takes 3 game objects (one for each lane), and 3 vector3s for each lane's offset. Added the LevelGen Script, which takes a float for the Z distance of the Map Pieces, and a list of Game Objects. Map Pieces put into the LevelGen list will be rendered into the game world in the order they are in the list, spaced by N units on the Z axis (N defined by float for Z distance).
    The Map system is currently only available in my Scene "Mitchell's Test Scene", or something like that, though the LevelGen script can be attached to any game object.

---
+++ Assets/Scripts/LevelGen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelGen : MonoBehaviour {
+	public float PieceZLength = 50;
+	public List<GameObject> PieceList;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < PieceList.Count; i+=1) {
+			GameObject.Instantiate(PieceList[i], new Vector3(0,0,i*PieceZLength), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/MapPiece.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPiece : MonoBehaviour {
+	public GameObject LeftLane, MiddleLane, RightLane;
+	public Vector3 LeftLaneOffset = new Vector3(-3,0,0),MiddleLaneOffset = new Vector3(0,0,0), RightLaneOffset = new Vector3(3,0,0);
+	// Use this for initialization
+	void Start () {
+		GameObject obj = (GameObject)GameObject.Instantiate(LeftLane,gameObject.transform.position + LeftLaneOffset,Quaternion.identity);
+		obj.transform.SetParent (gameObject.transform);
+		obj = (GameObject)GameObject.Instantiate(MiddleLane,gameObject.transform.position,Quaternion.identity);
+		obj.transform.SetParent (gameObject.transform);
+		obj = (GameObject)GameObject.Instantiate(RightLane,gameObject.transform.position + RightLaneOffset,Quaternion.identity);
+		obj.transform.SetParent (gameObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
