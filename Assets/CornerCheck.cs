6c88b9408249f4633ca0ba55d903958f1bd3913c
Mitchell Osborne <MOsborne@live.com.au>
2015-10-29 20:57:07 -0700

    Added working corners. The players position now locks to a position that lines up with the lanes when he corners.

---
+++ Assets/CornerCheck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornerCheck : MonoBehaviour {
+
+	public bool hasTurned = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if (Other.tag == "Player") {
+			//Can no longer change lanes
+		}
+	}
+
+	void OnTriggerExit(Collider Other)
+	{
+		if (Other.tag == "Player") {
+			//Can now change lanes
+		}
+	}
+}
---
+++ Assets/CornerPart.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornerPart : MonoBehaviour {
+	public CornerCheck cornerCheck;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider Other)
+	{
+		if (Other.tag == "Player") {
+			if(!cornerCheck.hasTurned)
+			{
+				if(Input.GetAxis ("Horizontal") > 0)
+				{
+					Other.transform.Rotate(new Vector3(0,90,0));
+					Other.transform.position = new Vector3(transform.position.x, Other.transform.position.y, transform.position.z);
+					cornerCheck.hasTurned = true;
+				}else if (Input.GetAxis ("Horizontal") < 0)
+				{
+					Other.transform.Rotate(new Vector3(0,-90,0));
+					Other.transform.position = new Vector3(transform.position.x, Other.transform.position.y, transform.position.z);
+					cornerCheck.hasTurned = true;
+				}
+			}
+		}
+	}
+}
