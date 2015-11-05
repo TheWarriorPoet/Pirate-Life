cf14d815635398d3c2ab51e41b2de03781dc920a
Mitchell Osborne <MOsborne@live.com.au>
2015-11-05 19:10:58 +1100

    Made some changes to the level editor hud, so it now lines up with different resolutions. Added extra functionality for the Level Editor - can now save Levels - Can now specify the offset for lane pieces.

--- Assets/PieceBuilderScript.cs
+++ Assets/PieceBuilderScript.cs
@@ -6,6 +6,7 @@ public class PieceBuilderScript : MonoBehaviour {
 
 	public MapPiece mapPiece;
 	public Dropdown LeftLaneDropDown, MidLaneDropDown, RightLaneDropDown;
+	public InputField LX, LY, LZ, MX, MY, MZ, RX, RY, RZ;
 	// Use this for initialization
 	void Start () {
 	
@@ -21,5 +22,19 @@ public class PieceBuilderScript : MonoBehaviour {
 		mapPiece.LeftLane = LeftLaneDropDown.GetComponent<MapPieceList> ().PieceList [LeftLaneDropDown.value];
 		mapPiece.MiddleLane = MidLaneDropDown.GetComponent<MapPieceList> ().PieceList [MidLaneDropDown.value];
 		mapPiece.RightLane = RightLaneDropDown.GetComponent<MapPieceList> ().PieceList [RightLaneDropDown.value];
+
+
+		try
+		{
+		mapPiece.LeftLaneOffset = new Vector3 (float.Parse(LX.text), float.Parse(LY.text), float.Parse(LZ.text));
+		mapPiece.MiddleLaneOffset = new Vector3 (float.Parse(MX.text), float.Parse(MY.text), float.Parse(MZ.text));
+		mapPiece.RightLaneOffset = new Vector3 (float.Parse(RX.text), float.Parse(RY.text), float.Parse(RZ.text));
+		}
+		catch(UnityException e)
+		{
+			mapPiece.LeftLaneOffset = new Vector3 (-3, 0, 0);
+			mapPiece.MiddleLaneOffset = new Vector3 (0, 0, 0);
+			mapPiece.RightLaneOffset = new Vector3 (3, 0, 0);
+		}
 	}
 }
