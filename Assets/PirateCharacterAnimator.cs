122c54b5302cbda6e306b3457c2a5f12a10cff2b
Weevilman <weevilman@live.com>
2016-01-14 19:43:47 +1000

    Fixed Player animations script, now responds to drunkenness to drive the blendspace

---
+++ Assets/PirateCharacterAnimator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PirateCharacterAnimator : MonoBehaviour {
+
+    public Player PiratePlayer;
+    public Animator PlayerAnimator;
+
+    float NewPlayerSpeedMult;
+
+
+    float LastDrunkenValue;
+    // Use this for initialization
+    void Start ()
+    {
+        PlayerAnimator = GetComponent<Animator>();
+    
+        PiratePlayer = GetComponentInParent<Player>();
+        PlayerAnimator.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //NewPlayerSpeedMult = (float)PiratePlayer.drunkenness / 100f;
+        NewPlayerSpeedMult = Mathf.Clamp(((float)PiratePlayer.drunkenness / 100f), 0, 1);
+        PlayerAnimator.SetFloat("PlayerSpeed", NewPlayerSpeedMult);
+       // Debug.Log(NewPlayerSpeedMult);
+
+    }
+
+    public void GoToRagdoll()
+    {
+        PlayerAnimator.enabled = false;
+    }
+
+}
