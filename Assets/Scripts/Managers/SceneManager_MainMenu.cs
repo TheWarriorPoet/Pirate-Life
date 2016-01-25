a6c59ecdb0ddf9519b69cf7fd0e9a2216641bb74
Zyte <zyte@zytebyte.com>
2016-01-26 00:30:33 +1030

    Moved coin counter from main menu to store. Improved look of menus.

--- Assets/Scripts/Managers/SceneManager_MainMenu.cs
+++ Assets/Scripts/Managers/SceneManager_MainMenu.cs
@@ -2,14 +2,10 @@
 using System.Collections;
 
 public class SceneManager_MainMenu : SceneManager_Base {
-    public UnityEngine.UI.Text m_CoinCounter = null;
 
     // Use this for initialization
     void Start () {
-	    if (m_CoinCounter != null && _myGameManager != null)
-        {
-            m_CoinCounter.text = "x " + _myGameManager.m_CoinScore;
-        }
+	    
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Managers/SceneManager_Store.cs
+++ Assets/Scripts/Managers/SceneManager_Store.cs
@@ -3,17 +3,30 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class SceneManager_Store : SceneManager_Base {
+	public UnityEngine.UI.Text m_CoinCounter = null;
+	public Button MagneticButton = null;
 
-    public Button MagneticButton = null;
+	private int coinCount;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (_myGameManager.magneticCoins)
+		{
+			MagneticButton.interactable = false;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (coinCount != _myGameManager.m_CoinScore)
+		{
+			coinCount = _myGameManager.m_CoinScore;
+
+			if (m_CoinCounter != null && _myGameManager != null)
+			{
+				m_CoinCounter.text = "x " + _myGameManager.m_CoinScore;
+			}
+		}
 	}
 
     public void PurchaseMagneticCoin(int cost)
