be0bde74dcedea4c5590d61b27efaecfaa8df22b
gcheffirs <gcheffirs@gmail.com>
2016-03-18 22:41:57 +1030

    Implemented updated movement system.
    Jumping uses the exact jump height, and the length of the jump is the current speed.
    Proper parabolic jump and fall arc while allowing more fast-paced movement.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
@@ -18,9 +19,8 @@ public class Player : MonoBehaviour
     public float minJumpHeight, maxJumpHeight;
     public float minLaneDelay, maxLaneDelay;
     public float laneDistance;
-	public float gravity = 9.8f;
 	public int currentLane;
-	public bool jumping, falling, grounded, isTurning;
+	public bool jumping, isTurning;
 	[Header("Touch Settings")]
 	public float deadzone = 0.8f;
 	[Header("Object Linking")]
@@ -37,16 +37,17 @@ public class Player : MonoBehaviour
     private AudioClip jumpSound, deckSound, landSound, splashSound, smackSound;
     // Controls
     private bool actionLeft, actionRight, actionJump;
-    // Drunkenness
-    private int prevDrunkenness;
+	private float jumpVelocity, jumpSpeed;
+	public Vector3 velocity;
+	List<Vector3> arc = new List<Vector3>();
+	// Drunkenness
+	private int prevDrunkenness;
     private int newDrunkenness;
     private float drunkTimer;
     // Lane Switching
     private float laneVelocity;
     private float lanePosition;
     private int previousLane;
-	// Jumping
-	private float jumpDistance;
     // Corner Turning
     private Vector3 cornerStart;
     private Vector3 cornerPoint;
@@ -99,6 +100,16 @@ public class Player : MonoBehaviour
             Controls();
             Action();
             Limits();
+
+			if (!jumping && controller.isGrounded)
+			{
+				if (Input.GetKeyDown(KeyCode.Space))
+				{
+					//velocity.y = controller.velocity.y;
+					jumpVelocity = jumpSpeed;
+					jumping = true;
+				}
+			}
 		}
 
         // Debug
@@ -193,13 +204,15 @@ public class Player : MonoBehaviour
                     currentLane++;
                 }
 
-                if (actionJump && !jumping && !falling)
+                if (actionJump && !jumping)
                 {
                     AudioSource.PlayClipAtPoint(jumpSound, transform.position);
 					AudioSource.PlayClipAtPoint(deckSound, transform.position);
 					anim.Play("Jumping");
+
+					jumpVelocity = jumpSpeed;
 					jumping = true;
-                }
+				}
                 break;
 
             case PlayerMode.TURNING:
@@ -308,27 +321,31 @@ public class Player : MonoBehaviour
 
     void Movement()
     {
-		Vector3 vel = Vector3.zero;
+		// Debug
+		arc.Add(transform.position);
+
+		if (arc.Count > 100)
+		{
+			arc.RemoveAt(0);
+		}
+
+		for (int i = 0; i < arc.Count - 1; i++)
+		{
+			Color c = arc[i].y < 0 ? Color.red : Color.green;
+
+			Debug.DrawLine(arc[i], arc[i + 1], c);
+			Debug.DrawLine(arc[i], arc[i] - transform.up, Color.grey);
+		}
 
 		// Camera effects
 		mainCamera.fieldOfView = 60.0f + drunkenness / 5.0f;
-		mainCamera.transform.localEulerAngles = new Vector3(15, 0, laneVelocity * drunkenness / 75.0f);
 
-		// Falling
-		float dist = 1.0f;
-		Vector3 dir = new Vector3(0, -1, 0);
+		Vector3 cameraLean = mainCamera.transform.localEulerAngles;
+		cameraLean.z = laneVelocity * drunkenness / 75.0f;
+		mainCamera.transform.localEulerAngles = cameraLean;
 
-		Debug.DrawRay(transform.position, dir * dist, Color.green);
-		if (!Physics.Raycast(transform.position, dir, dist))
-		{
-			if (!jumping)
-			{
-				falling = true;
-			}
-		}
-
-        // Drunkenness
-        if (drunkenness != newDrunkenness)
+		// Drunkenness
+		if (drunkenness != newDrunkenness)
         {
             drunkTimer += Time.deltaTime / drunkDelay;
             drunkenness = (int)Mathf.Lerp(prevDrunkenness, newDrunkenness, drunkTimer);
@@ -339,51 +356,57 @@ public class Player : MonoBehaviour
 
         lanePosition = Mathf.SmoothDamp(lanePosition, currentLane, ref laneVelocity, laneDelay);
 
-		vel += transform.right * (laneVelocity * laneDistance) * Time.deltaTime;
+		Vector3 vel = transform.right * (laneVelocity * laneDistance) * Time.deltaTime;
 
-        // Leaning
-        transform.Find("Pirate_Character").localEulerAngles = new Vector3(0, 0, -laneVelocity * (1.0f + drunkenness / 20.0f));
+		controller.Move(vel);
+
+		// Leaning
+		transform.Find("Pirate_Character").localEulerAngles = new Vector3(0, 0, -laneVelocity * (1.0f + drunkenness / 20.0f));
 
 		// Running
 		float runSpeed = Mathf.Lerp(minRunSpeed, maxRunSpeed, drunkenness / 100.0f);
 
-		vel += (transform.forward * runSpeed) * Time.deltaTime;
+		velocity = transform.forward * runSpeed;
 
-        if (sceneManager != null)
+		if (sceneManager != null)
         {
-            sceneManager.m_Distance += runSpeed;
+            sceneManager.m_Distance += runSpeed * Time.deltaTime;
         }
 
         // Jumping
         float jumpHeight = Mathf.Lerp(minJumpHeight, maxJumpHeight, drunkenness / 100.0f);
 
-        if (jumping)
-        {
-			float jump = gravity * Time.deltaTime;
+		/*
+		s = jumpHeight
+		u = u
+		v = 0
+		a = gravity
+		t = ~
 
-			jumpDistance += jump;
+		Equation without t is:
 
-			if (jumpDistance > jumpHeight)
-			{
-				jumping = false;
-				jumpDistance = 0;
-			}
-			else
-			{
-				vel.y += jump;
-			}
+		u^2 = v^2 - 2as
 
-			Debug.Log(jumpDistance);
-        }
+		hence as v = 0 (we want speed to be 0 so that its at the 'top' of the jump)
+
+		u = sqrt(-2as)
+		*/
+
+		float gravity = 10.0f * (jumpHeight * 0.85f);
+
+		jumpSpeed = Mathf.Sqrt(2.0f * gravity * jumpHeight);
+
+		velocity.y = jumpVelocity;
 
 		// Falling
-		if (falling)
+		if (!controller.isGrounded)
 		{
-			vel.y -= gravity * Time.deltaTime;
+			jumpVelocity -= gravity * Time.deltaTime;
 		}
 
-		controller.Move(vel);
-    }
+		controller.Move(velocity * Time.deltaTime);
+
+	}
 
 	void KillCharacter()
 	{
@@ -412,16 +435,18 @@ public class Player : MonoBehaviour
 			KillCharacter();
 		}
 
-		if (falling)
+		if (jumping && !controller.isGrounded)
 		{
 			if (!ragdolled)
 			{
 				AudioSource.PlayClipAtPoint(landSound, transform.position);
 				anim.Play("Falling");
 			}
-			falling = false;
 		}
-    }
+
+		jumpVelocity = 0;
+		jumping = false;
+	}
 
     void OnTriggerEnter(Collider collider)
     {
@@ -489,8 +514,6 @@ public class Player : MonoBehaviour
         turnDegree = 0;
 
         jumping = false;
-		falling = false;
-		grounded = false;
 
         drunkenness = 0;
         drunkTimer = 0;
