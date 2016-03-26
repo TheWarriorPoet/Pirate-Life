7e2213f39d77a0c95038d58799b436a591ca83b1
Mitchell Osborne <MOsborne@live.com.au>
2016-03-26 14:05:59 +1100

    Readded corner generation to the Level Generator, Increased Player Move speed from 12 to 15. (This makes the game feel slighty more fast paced, and the player can jump gaps with 0 drunkeness easier).

--- Assets/Scripts/Generation/LevelGen.cs
+++ Assets/Scripts/Generation/LevelGen.cs
@@ -7,6 +7,7 @@ public class LevelGen : MonoBehaviour {
 	public float PieceLength = 50;
 	//public GameObject CornerBlock; //This is to check if the block is a corner, and to start layering the track around the corner
 	public List<GameObject> SectionList;
+	public GameObject LCorner, RCorner;
 	private List<GameObject> objList = new List<GameObject>();
 	private Vector3 TrackPos = new Vector3(0,0,0);
 	// Use this for initialization
@@ -17,6 +18,33 @@ public class LevelGen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	private void AddCorner()
+	{
+
+		Random.seed = (int)System.DateTime.Now.Ticks;
+		int i = Random.Range (0, 99);
+		GameObject obj;
+		if (i <= 50) {
+			obj = (GameObject)GameObject.Instantiate (RCorner, TrackPos, Quaternion.Euler (0, TrackDirection, 0));
+			obj.GetComponent<Transform> ().rotation.Set (0, TrackDirection, 0, 0);
+			obj.transform.SetParent (gameObject.transform);
+			objList.Add (obj);
+			TrackDirection -= 90;
+			if (TrackDirection <= -90) {
+				TrackDirection = 270;
+			}
+		} else {
+			obj = (GameObject)GameObject.Instantiate (LCorner, TrackPos, Quaternion.Euler (0, TrackDirection, 0));
+			obj.GetComponent<Transform> ().rotation.Set (0, TrackDirection, 0, 0);
+			obj.transform.SetParent (gameObject.transform);
+			objList.Add (obj);
+			TrackDirection += 90;
+			if (TrackDirection >= 360) {
+				TrackDirection = 0;
+			}
+		}
 	}
 
 	public void RebuildMap()
@@ -35,25 +63,6 @@ public class LevelGen : MonoBehaviour {
 				obj.GetComponent<Transform> ().rotation.Set (0, TrackDirection, 0, 0);
 				obj.transform.SetParent (gameObject.transform);
 				objList.Add (obj);
-				//Checks if the current piece is a corner, if so:
-				if (SectionList [i].tag == "Corner") {
-					//If the piece is flagged as a Left Corner:
-					if (SectionList [i].GetComponent<CornerCheck> ().LeftCorner) {
-						//If the Current Direction Isn't North, reduce it by 90, if it is north, set it to west (because West is 270, and wont wirh with -90)
-					
-						TrackDirection -= 90;
-						if (TrackDirection == -90) {
-							TrackDirection = 270;
-						}
-					} else {
-						//If the Current Direction isn't West, Add 90 to it, if it is, set it to North (as North = 0, not 360)
-					
-						TrackDirection += 90;
-						if (TrackDirection == 360) {
-							TrackDirection = 0;
-						}
-					}
-				}
 			
 				//Adjusts the Position of the next block position based on what direction we're heading in
 				switch (TrackDirection) {
@@ -73,6 +82,23 @@ public class LevelGen : MonoBehaviour {
 					break;
 				}
 			
+			}
+			AddCorner ();
+			switch (TrackDirection) {
+			case 0:
+				TrackPos.z += PieceLength;
+				break;
+			case 90:
+				TrackPos.x += PieceLength;
+				break;
+			case 180:
+				TrackPos.z -= PieceLength;
+				break;
+			case 270:
+				TrackPos.x -= PieceLength;
+				break;
+			default:
+				break;
 			}
 		}
 		TrackDirection = 0;
