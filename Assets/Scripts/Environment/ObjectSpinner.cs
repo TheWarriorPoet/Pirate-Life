0bc520fd8eca43b0efaddf8450beffa3adc2baff
gcheffirs <gcheffirs@gmail.com>
2016-03-27 14:07:04 +1030

    Added spin script to Clouds layer. Some script organisation.

---
+++ Assets/Scripts/Environment/ObjectSpinner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpinner : MonoBehaviour
+{
+	public float spinSpeed;
+	public bool randomStartRotation;
+
+	void Start()
+	{
+		if (randomStartRotation)
+		{
+			transform.Rotate(transform.up, Random.Range(0.0f, 360.0f));
+		}
+	}
+
+	void Update()
+	{
+		transform.Rotate(transform.up, spinSpeed * Time.deltaTime);
+	}
+}
