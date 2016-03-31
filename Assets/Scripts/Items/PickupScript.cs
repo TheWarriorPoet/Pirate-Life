289d2f4aa97b7dbb09c12b7a1f58b0a25394bf99
gcheffirs <gcheffirs@gmail.com>
2016-03-31 17:16:13 +1030

    Collectibles fade when behind player.

--- Assets/Scripts/Items/PickupScript.cs
+++ Assets/Scripts/Items/PickupScript.cs
@@ -12,7 +12,8 @@ public class PickupScript : MonoBehaviour
 {
 	public PickupType pickupType;
 	private Player player;
-    private Transform playerTransform = null;
+	private Renderer renderer;
+	private Transform playerTransform = null;
     private Transform objectTransform = null;
     private SceneManager_Andrew _mySceneManager = null;
 	private AudioClip soundEffect;
@@ -25,7 +26,7 @@ public class PickupScript : MonoBehaviour
         player = temp.GetComponent<Player>();
         playerTransform = temp.transform;
         _mySceneManager = GameObject.Find("SceneManager").GetComponent<SceneManager_Andrew>();
-
+		renderer = GetComponent<Renderer>();
 
 		// Set sound effect
 		switch (pickupType)
@@ -45,8 +46,20 @@ public class PickupScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		Vector3 directionToTarget = transform.position - player.transform.position;
+		float angle = Vector3.Angle(player.transform.forward, directionToTarget);
 
-    }
+		if (Mathf.Abs(angle) > 90 && Mathf.Abs(angle) < 270)
+		{
+			SetAlpha(0.5f);
+			//Debug.Log("target is in front of me");
+		}
+		if (Mathf.Abs(angle) < 90 || Mathf.Abs(angle) > 270)
+		{
+			SetAlpha(1.0f);
+			//Debug.Log("target is behind me");
+		}
+	}
 
 	void OnTriggerEnter(Collider Other)
 	{
@@ -73,6 +86,21 @@ public class PickupScript : MonoBehaviour
 					break;
 			}
 			Destroy(gameObject);
+		}
+	}
+
+	void SetAlpha(float value)
+	{
+		if (value < 1.0f)
+		{
+			renderer.material.shader = Shader.Find("Transparent/Diffuse");
+			Color c = renderer.material.color;
+			c.a = value;
+			renderer.material.color = c;
+		}
+		else
+		{
+			renderer.material.shader = Shader.Find("Mobile/Diffuse");
 		}
 	}
 }
