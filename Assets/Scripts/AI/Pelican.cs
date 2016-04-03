da007958682e0c17509b7cbd5a48a719f43febed
gcheffirs <gcheffirs@gmail.com>
2016-04-04 05:52:36 +0930

    Pelican hazardous projectile vomit system engaged.

--- Assets/Scripts/AI/Pelican.cs
+++ Assets/Scripts/AI/Pelican.cs
@@ -3,13 +3,54 @@ using System.Collections;
 
 public class Pelican : MonoBehaviour
 {
+	public GameObject pukePrefab;
+	public float activateRadius, pukeDistance, pukeDuration;
+	public bool hasPuked;
+	Player player;
+	ParticleSystem vomit;
+	float pukeTimer;
+
 	void Start()
 	{
-
+		player = FindObjectOfType<Player>();
+		vomit = GetComponentInChildren<ParticleSystem>();
+		vomit.gameObject.SetActive(false);
 	}
 
 	void Update()
 	{
+		if (player == null) return;
 
+		float distance = Vector3.Distance(transform.position, player.transform.position);
+
+		if (!hasPuked && distance < activateRadius)
+		{
+			SpawnPuke();
+		}
+
+		if (hasPuked && vomit.gameObject.activeInHierarchy)
+		{
+			pukeTimer += Time.deltaTime;
+			if (pukeTimer >= pukeDuration)
+			{
+				vomit.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	void SpawnPuke()
+	{
+		Vector3 target = (transform.position + transform.up) - transform.right * pukeDistance;
+
+		Debug.DrawLine(target, target - transform.up * 10);
+
+		RaycastHit hit;
+		if (Physics.Raycast(target, -transform.up, out hit, 10))
+		{
+			Instantiate(pukePrefab, hit.point, Quaternion.identity);
+			Debug.Log("PUKE!");
+			hasPuked = true;
+			vomit.gameObject.SetActive(true);
+		}
 	}
 }
