10794f33a24e599018c623f1f98d4c72905de0a7
Mitchell Osborne <MOsborne@live.com.au>
2016-04-18 14:26:35 +1000

    Added posts automatically spawning onto the generated pieces. Updated all the existing MapPieces to have a link to the Posts prefab.
    New Pieces should automatically have the Posts prefab assigned, however its possible that it wont, Ill have to double check.

--- Assets/Scripts/Generation/MapPiece.cs
+++ Assets/Scripts/Generation/MapPiece.cs
@@ -6,30 +6,12 @@ public class MapPiece : MonoBehaviour {
 	public List<Vector3> LaneOffsets;
 	public List<GameObject> Collectables;
 	public List<GameObject> Obstacles;
+	public GameObject Posts;
 	private List<GameObject> objs = new List<GameObject> ();
 	// Use this for initialization
 	void Start () {
 
-		foreach (GameObject obj in objs) {
-			Destroy (obj);
-		}
-
-		objs.Clear ();
-		objs.Add((GameObject)GameObject.Instantiate(Lanes[0],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[0]),gameObject.transform.rotation));
-		objs.Add((GameObject)GameObject.Instantiate(Lanes[1],gameObject.transform.position + LaneOffsets[1],gameObject.transform.rotation));
-		objs.Add((GameObject)GameObject.Instantiate(Lanes[2],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[2]),gameObject.transform.rotation));
-
-		objs.Add((GameObject)GameObject.Instantiate(Collectables[0],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[0]),gameObject.transform.rotation));
-		objs.Add((GameObject)GameObject.Instantiate(Collectables[1],gameObject.transform.position + LaneOffsets[1],gameObject.transform.rotation));
-		objs.Add((GameObject)GameObject.Instantiate(Collectables[2],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[2]),gameObject.transform.rotation));
-
-		objs.Add((GameObject)GameObject.Instantiate(Obstacles[0],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[0]),gameObject.transform.rotation));
-		objs.Add((GameObject)GameObject.Instantiate(Obstacles[1],gameObject.transform.position + LaneOffsets[1],gameObject.transform.rotation));
-		objs.Add((GameObject)GameObject.Instantiate(Obstacles[2],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[2]),gameObject.transform.rotation));
-
-		foreach (GameObject obj in objs) {
-			obj.transform.SetParent (gameObject.transform);
-		}
+		BuildPiece ();
 		//GetComponent<PickupLayout> ().RebuildCollectables ();
 	}
 	
@@ -46,6 +28,23 @@ public class MapPiece : MonoBehaviour {
 		}
 
 		objs.Clear ();
+		if (Lanes [0].tag != "Empty") {
+			objs.Add ((GameObject)GameObject.Instantiate (Posts, gameObject.transform.position + (gameObject.transform.rotation * (LaneOffsets [0] - new Vector3(1.5f,0,0))), gameObject.transform.rotation));
+		} else if (Lanes [1].tag != "Empty") {
+			objs.Add ((GameObject)GameObject.Instantiate (Posts, gameObject.transform.position + (gameObject.transform.rotation * (LaneOffsets [1] - new Vector3(1.5f,0,0))), gameObject.transform.rotation));
+		} else if (Lanes [2].tag != "Empty") {
+			objs.Add ((GameObject)GameObject.Instantiate (Posts, gameObject.transform.position + (gameObject.transform.rotation * (LaneOffsets [2] - new Vector3(1.5f,0,0))), gameObject.transform.rotation));
+		}
+
+		if (Lanes [2].tag != "Empty") {
+			objs.Add ((GameObject)GameObject.Instantiate (Posts, gameObject.transform.position + (gameObject.transform.rotation * (LaneOffsets [2] + new Vector3(1.5f,0,0))), gameObject.transform.rotation));
+		} else if (Lanes [1].tag != "Empty") {
+			objs.Add ((GameObject)GameObject.Instantiate (Posts, gameObject.transform.position + (gameObject.transform.rotation * (LaneOffsets [1] + new Vector3(1.5f,0,0))), gameObject.transform.rotation));
+		} else if (Lanes [0].tag != "Empty") {
+			objs.Add ((GameObject)GameObject.Instantiate (Posts, gameObject.transform.position + (gameObject.transform.rotation * (LaneOffsets [0] + new Vector3(1.5f,0,0))), gameObject.transform.rotation));
+		}
+
+
 		objs.Add((GameObject)GameObject.Instantiate(Lanes[0],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[0]),gameObject.transform.rotation));
 		objs.Add((GameObject)GameObject.Instantiate(Lanes[1],gameObject.transform.position + LaneOffsets[1],gameObject.transform.rotation));
 		objs.Add((GameObject)GameObject.Instantiate(Lanes[2],gameObject.transform.position + (gameObject.transform.rotation * LaneOffsets[2]),gameObject.transform.rotation));
