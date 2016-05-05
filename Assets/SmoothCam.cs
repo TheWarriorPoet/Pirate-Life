40b2685a0e36ec7ec06257c77b89af9f8a86872b
gcheffirs <gcheffirs@gmail.com>
2016-05-05 18:30:48 +0930

    Added death cam to track ragdoll.

--- Assets/SmoothCam.cs
+++ Assets/SmoothCam.cs
@@ -6,6 +6,8 @@ public class SmoothCam : MonoBehaviour
 	public float rotationSpeed;
 	public float positionSpeed;
 	public bool followPlayer;
+	public GameObject deathTarget;
+	public bool deathCam;
 
 	Player player;
 	Vector3 posOffset;
@@ -32,22 +34,48 @@ public class SmoothCam : MonoBehaviour
 		{
 			CalculateTarget();
 
-			// Move smoothly to target
-			transform.position = Vector3.Lerp(transform.position, targetPos, positionSpeed * Time.deltaTime);
-			//transform.position = targetPos;
-			transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
-			//transform.rotation = targetRot;
-
-			// Camera effects
-			cam.fieldOfView = 60.0f + player.drunkenness / 5.0f;
-
-			Vector3 cameraLean = cam.transform.localEulerAngles;
-			cameraLean.z = player.GetLaneVelocity() * player.drunkenness / 75.0f;
-			cam.transform.localEulerAngles = cameraLean;
-
-			// Debug
-			Debug.DrawLine(player.transform.position, targetPos);
+			FollowTarget();
 		}
+		else if (deathCam)
+		{
+			TrackDeath();
+		}
+
+		// Debug
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			ResetCam();
+		}
+	}
+
+	void FollowTarget()
+	{
+		// Move smoothly to target
+		transform.position = Vector3.Lerp(transform.position, targetPos, positionSpeed * Time.deltaTime);
+		//transform.position = targetPos;
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
+		//transform.rotation = targetRot;
+
+		// Camera effects
+		cam.fieldOfView = 60.0f + player.drunkenness / 5.0f;
+
+		Vector3 cameraLean = cam.transform.localEulerAngles;
+		cameraLean.z = player.GetLaneVelocity() * player.drunkenness / 75.0f;
+		cam.transform.localEulerAngles = cameraLean;
+
+		// Debug
+		Debug.DrawLine(player.transform.position, targetPos);
+	}
+
+	void TrackDeath()
+	{
+		if (deathTarget == null)
+		{
+			return;
+		}
+
+		targetRot = Quaternion.LookRotation(deathTarget.transform.position - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotationSpeed);
 	}
 
 	void CalculateTarget()
