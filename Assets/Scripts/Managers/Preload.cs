2448ac7d83f69a7b308e43c8444f4995ac81d151
Andrew Jones <info@warriorgames.com.au>
2016-05-23 17:28:57 +1000

    Fix for main menu

---
+++ Assets/Scripts/Managers/Preload.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Preload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene("Main Menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
