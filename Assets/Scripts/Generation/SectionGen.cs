fc4a5da1b407d9094e9241e2957598c94b2e44cf
Mitchell Osborne <MOsborne@live.com.au>
2016-05-30 20:58:38 +1000

    Fixing whacky crap with random stuff in my code.

--- Assets/Scripts/Generation/SectionGen.cs
+++ Assets/Scripts/Generation/SectionGen.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 
 public class SectionGen : MonoBehaviour {
 	public bool RandomGen;
-<<<<<<< HEAD
-	public int MaxSectionSize, MinSectionSize;
-=======
->>>>>>> 4b15c46... Added cannons to the procgenerator, these fire cannon balls when the player moves within range,
-	public List<GameObject> FullPieceList = new List<GameObject> ();
+	public int MaxSectionSize, MinSectionSize;public List<GameObject> FullPieceList = new List<GameObject> ();
 	public List<GameObject> PieceList = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
 		if (RandomGen) {
-<<<<<<< HEAD
 			GenerateSection ();
 		}
 	}
@@ -30,7 +25,6 @@ public class SectionGen : MonoBehaviour {
 				List<GameObject> tempList = PieceList [PieceList.Count - 1].GetComponent<MapPiece> ().NextPieces;
 				PieceList.Add (tempList [Random.Range (0, tempList.Count - 1)]);
 			}
-=======
 			GenerateSection (3);
 		}
 	}
@@ -43,7 +37,6 @@ public class SectionGen : MonoBehaviour {
 		for (int i = 1; i < Size; i++) {
 			List<GameObject> tempList = PieceList[PieceList.Count - 1].GetComponent<MapPiece>().NextPieces;
 			PieceList.Add (tempList [Random.Range (0, tempList.Count - 1)]);
->>>>>>> 4b15c46... Added cannons to the procgenerator, these fire cannon balls when the player moves within range,
 		}
 	}
 	
