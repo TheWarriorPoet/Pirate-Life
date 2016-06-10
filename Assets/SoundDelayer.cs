2ed0968d2348d3aed9e1b450208a53a30e2a0727
gcheffirs <gcheffirs@gmail.com>
2016-06-11 00:49:20 +0930

    Decreased seagull count and offset their sound from each other. Also added some delay instead of immediate looping.

---
+++ Assets/SoundDelayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDelayer : MonoBehaviour
+{
+	public float timeDelay;
+	public bool random;
+
+	AudioSource sound;
+	float delay, delayTimer;
+
+	void Start()
+	{
+		sound = GetComponent<AudioSource>();
+		delay = random ? RandomDelay() : timeDelay;
+	}
+
+	void Update()
+	{
+		if (sound.isPlaying) return;
+
+		delayTimer += Time.deltaTime;
+
+		if (delayTimer >= delay)
+		{
+			sound.Play();
+			delayTimer = 0;
+		}
+	}
+
+	float RandomDelay()
+	{
+		return Random.Range(0.0f, timeDelay);
+	}
+}
