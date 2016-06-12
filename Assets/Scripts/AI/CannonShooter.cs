55933a6844d5fc517406bcb8595b533f0aab141b
gcheffirs <gcheffirs@gmail.com>
2016-06-13 03:09:19 +0930

    Made cannons ALWAYS intercept player.

--- Assets/Scripts/AI/CannonShooter.cs
+++ Assets/Scripts/AI/CannonShooter.cs
@@ -5,28 +5,52 @@ public class CannonShooter : MonoBehaviour
 {
 	public GameObject ballPrefab, effectPrefab;
 	public float activateRadius;
-	public bool hasFired;
+	public bool sideCannon, hasFired;
 
 	AudioSource audioSrc;
 	Player player;
+	Vector3 testStart, testDir;
+	LayerMask lm;
 
 	void Start()
 	{
+		lm = (1 << LayerMask.NameToLayer("Player"));
 		audioSrc = GetComponent<AudioSource>();
 		player = FindObjectOfType<Player>();
+
+		testStart = transform.parent.position;
+		testStart.y += 4;
+		testDir = (transform.parent.right - transform.parent.forward).normalized;
 	}
 
 	void Update()
 	{
-		float distance = Vector3.Distance(transform.position, player.transform.position);
-
-		if (!hasFired && distance < activateRadius)
+		if (sideCannon)
 		{
-			Instantiate(ballPrefab, transform.position, transform.rotation);
-			Instantiate(effectPrefab, transform.position, transform.rotation);
-			audioSrc.Play();
-			hasFired = true;
-			Debug.Log("BOMBS AWAY!");
+			Debug.DrawLine(testStart, testStart + testDir * 30);
+
+			if (!hasFired && Physics.Raycast(testStart, testDir, 30.0f, lm)) //if (!hasFired && distance < activateRadius)
+			{
+				Shoot();
+			}
 		}
+		else
+		{
+			float distance = Vector3.Distance(transform.position, player.transform.position);
+
+			if (!hasFired && distance < activateRadius)
+			{
+				Shoot();
+			}
+		}
+	}
+
+	void Shoot()
+	{
+		Instantiate(ballPrefab, transform.position, transform.rotation);
+		Instantiate(effectPrefab, transform.position, transform.rotation);
+		audioSrc.Play();
+		hasFired = true;
+		Debug.Log("BOMBS AWAY!");
 	}
 }
